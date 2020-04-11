47bca01da8b7425894799b8c7910bb895e3deb6f
Khoa Phan <khoa.phan.9xset@gmail.com>
2020-04-11 12:37:17 +0700

    update folder name

--- scripts/backup_influxdb.sh
+++ scripts/backup_influxdb.sh
@@ -2,10 +2,10 @@
 
 #first move the contents of the old backup out and clear the directory
 echo "Moving old influxdb backups if they exist"
-[ -d ~/IOTstack/backups/influxdb/db_old ] || sudo mkdir ~/IOTstack/backups/influxdb/db_old
-sudo rm ~/IOTstack/backups/influxdb/db_old/* >/dev/null 2>&1
-sudo mv ~/IOTstack/backups/influxdb/db/* ~/IOTstack/backups/influxdb/db_old/ >/dev/null 2>&1
-#sudo rm ~/IOTstack/backups/influxdb/db/*
+[ -d ~/IOTStack/backups/influxdb/db_old ] || sudo mkdir ~/IOTStack/backups/influxdb/db_old
+sudo rm ~/IOTStack/backups/influxdb/db_old/* >/dev/null 2>&1
+sudo mv ~/IOTStack/backups/influxdb/db/* ~/IOTStack/backups/influxdb/db_old/ >/dev/null 2>&1
+#sudo rm ~/IOTStack/backups/influxdb/db/*
 
 #execute the backup command
 echo "backing up influxdb database"
